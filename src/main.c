9441fd3d50111b0fe246d397812a71bd46537173
leijin4wk <leijin4wk@163.com>
2019-06-27 21:29:24 +0800

    验证条件编译功能

--- src/main.c
+++ src/main.c
@@ -7,6 +7,19 @@
 extern struct epoll_event *events;
 
 int main() {
+
+#ifdef USE_A
+    printf("USE_A\n");
+#else
+    printf("NOT USE_A\n");
+#endif
+
+#ifdef USE_B
+    printf("USE_B\n");
+#else
+    printf("NOT USE_B\n");
+#endif
+
     int socket_fd=create_and_bind(8888);
     check_exit(socket_fd<0,"socket 创建和绑定失败！");
 
@@ -29,24 +42,22 @@ int main() {
 
     m_epoll_add(epoll_fd, listen_fd, &event);
 
-    while (1) {
-        int n = m_epoll_wait(epoll_fd, events, MAXEVENTS, -1);
-        for (int i = 0; i < n; i++){
-
-            if ((events[i].events & EPOLLERR) ||(events[i].events & EPOLLHUP) ||(!(events[i].events & EPOLLIN))){
-                fprintf (stderr, "epoll error\n");
-                close (events[i].data.fd);
-                continue;
-            } else if (listen_fd == events[i].data.fd){
-                printf("我来了！\n");
-            }else{
-                printf("哈哈哈！\n");
-            }
-
-        }
-    }
-
-
+//    while (1) {
+//        int n = m_epoll_wait(epoll_fd, events, MAXEVENTS, -1);
+//        for (int i = 0; i < n; i++){
+//
+//            if ((events[i].events & EPOLLERR) ||(events[i].events & EPOLLHUP) ||(!(events[i].events & EPOLLIN))){
+//                fprintf (stderr, "epoll error\n");
+//                close (events[i].data.fd);
+//                continue;
+//            } else if (listen_fd == events[i].data.fd){
+//                printf("我来了！\n");
+//            }else{
+//                printf("哈哈哈！\n");
+//            }
+//
+//        }
+//    }
 
     printf("aaa\n");
     return 0;
