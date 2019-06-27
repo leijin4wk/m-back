523049beb56170cbd6684a2cc62cb7666293fa0c
leijin4wk <leijin4wk@163.com>
2019-06-27 19:41:58 +0800

    添加socket相关函数

--- src/epoll.c
+++ src/epoll.c
@@ -12,33 +12,33 @@ struct epoll_event *events;
 
 int m_epoll_create(int flags) {
     int fd = epoll_create1(flags);
-    check(fd > 0, "zv_epoll_create: epoll_create1");
+    check(fd < 0, "epoll_create: epoll_create1");
 
     events = (struct epoll_event *)malloc(sizeof(struct epoll_event) * MAXEVENTS);
-    check(events != NULL, "zv_epoll_create: malloc");
+    check(events == NULL, "epoll_create: malloc");
     return fd;
 }
 
 void m_epoll_add(int epfd, int fd, struct epoll_event *event) {
     int rc = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, event);
-    check(rc == 0, "zv_epoll_add: epoll_ctl");
+    check(rc != 0, "epoll_add: epoll_ctl");
     return;
 }
 
 void m_epoll_mod(int epfd, int fd, struct epoll_event *event) {
     int rc = epoll_ctl(epfd, EPOLL_CTL_MOD, fd, event);
-    check(rc == 0, "zv_epoll_mod: epoll_ctl");
+    check(rc != 0, "epoll_mod: epoll_ctl");
     return;
 }
 
 void m_epoll_del(int epfd, int fd, struct epoll_event *event) {
     int rc = epoll_ctl(epfd, EPOLL_CTL_DEL, fd, event);
-    check(rc == 0, "zv_epoll_del: epoll_ctl");
+    check(rc != 0, "epoll_del: epoll_ctl");
     return;
 }
 
 int m_epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout) {
     int n = epoll_wait(epfd, events, maxevents, timeout);
-    check(n >= 0, "zv_epoll_wait: epoll_wait");
+    check(n < 0, "epoll_wait: epoll_wait");
     return n;
 }
--- src/main.c
+++ src/main.c
@@ -1,13 +1,53 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "socket.h"
 #include "dbg.h"
+#include "epoll.h"
+
+extern struct epoll_event *events;
 
 int main() {
-    int flag,socket_fd,listenfd;
-    socket_fd=create_and_bind(8888);
-    listenfd=socket_listen(socket_fd);
-    flag = make_socket_non_blocking(listenfd);
-    check(flag == 0, "make_socket_non_blocking");
+    int socket_fd=create_and_bind(8888);
+    check_exit(socket_fd<0,"socket 创建和绑定失败！");
+
+    int listen_fd=socket_listen(socket_fd);
+
+    check_exit(listen_fd<0,"socket 监听失败！");
+
+    int flag = make_socket_non_blocking(listen_fd);
+
+    check_exit(flag <0, "设置socket为非阻塞失败！");
+
+    int epoll_fd = m_epoll_create(0);
+
+    check_exit(epoll_fd <0, "创建epoll失败！");
+
+    struct epoll_event event;
+    event.data.fd = listen_fd;
+    //读入,边缘触发方式
+    event.events = EPOLLIN | EPOLLET;
+
+    m_epoll_add(epoll_fd, listen_fd, &event);
+
+    while (1) {
+        int n = m_epoll_wait(epoll_fd, events, MAXEVENTS, -1);
+        for (int i = 0; i < n; i++){
+
+            if ((events[i].events & EPOLLERR) ||(events[i].events & EPOLLHUP) ||(!(events[i].events & EPOLLIN))){
+                fprintf (stderr, "epoll error\n");
+                close (events[i].data.fd);
+                continue;
+            } else if (listen_fd == events[i].data.fd){
+                printf("我来了！\n");
+            }else{
+                printf("哈哈哈！\n");
+            }
+
+        }
+    }
+
+
+
     printf("aaa\n");
     return 0;
 }
