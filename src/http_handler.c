92eeaecdd6694a82603fdc9d71f7cc2606eb24ca
leijin4wk <leijin4wk@163.com>
2019-06-28 18:08:08 +0800

    测试http打印

--- src/http_handler.c
+++ src/http_handler.c
@@ -1,11 +1,20 @@
 //
 // Created by oyo on 2019-06-28.
 //
+#include <sys/socket.h>
 #include "http_handler.h"
 #include "dbg.h"
+#include "http_parser.h"
 
 void handler_request(void *ptr) {
     int* fd=(int*)ptr;
     log_info("handler request fd %d", *fd);
-
+    size_t len = 80*1024;
+    char buf[len];
+    ssize_t recved;
+    recved = recv(*fd, buf, len, 0);
+    if (recved < 0) {
+        log_err("recv err!");
+    }
+    log_info("%s\n",buf);
 }
