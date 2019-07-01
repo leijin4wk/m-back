bdc0975834a476e26bb6071eb4de69a7b1f2bb9d
leijin4wk <leijin4wk@163.com>
2019-07-01 14:54:19 +0800

    修改

--- src/http.c
+++ src/http.c
@@ -5,44 +5,90 @@
 #include "http_parser.h"
 #include "http.h"
 #include "dbg.h"
-//http://ju.outofmemory.cn/entry/111438
-// http_cb      on_message_begin;
-//  http_data_cb on_url;
-//  http_data_cb on_status;
-//  http_data_cb on_header_field;
-//  http_data_cb on_header_value;
-//  http_cb      on_headers_complete;
-//  http_data_cb on_body;
-//  http_cb      on_message_complete;
-//  /* When on_chunk_header is called, the
-//  current chunk length is stored
-//   * in parser->content_length.
-//   */
-//  http_cb      on_chunk_header;
-//  http_cb      on_chunk_complete;
+static struct http_request *new_http_request();
+static void delete_http_request(struct http_request *request);
+static struct http_header *new_http_header();
+static void delete_http_header(struct http_header *header);
+static inline struct http_header *add_http_header(struct http_request *request);
+// 初始化一个新的HTTP请求
+static struct http_request *new_http_request() {
+    struct http_request *request = malloc(sizeof(struct http_request));
+    request->headers = NULL;
+    request->url = NULL;
+    request->body = NULL;
+    request->flags = 0;
+    request->http_major = 0;
+    request->http_minor = 0;
+    return request;
+}
+// 删除一个HTTP请求
+static  void delete_http_request(struct http_request *request) {
+    if (request->url != NULL) free(request->url);
+    if (request->body != NULL) free(request->body);
+    struct http_header *header = request->headers;
+    while (header != NULL) {
+        struct http_header *to_delete = header;
+        header = header->next;
+        delete_http_header(to_delete);
+    }
+    free(request);
+}
+// 初始化一个新的头结点
+static struct http_header *new_http_header() {
+    struct http_header *header = malloc(sizeof(struct http_header));
+    header->name = NULL;
+    header->value = NULL;
+    header->next = NULL;
+    return header;
+}
+//释放一个头结点内存
+static void delete_http_header(struct http_header *header) {
+    if (header->name != NULL) free(header->name);
+    if (header->value != NULL) free(header->value);
+    free(header);
+}
+// 将一个空的HTTP头部字段附件到字段链表的尾部
+// 返回值为创建的新节点
+static struct http_header *add_http_header(struct http_request *request) {
+    struct http_header *header = request->headers;
+    // 从头开始循环链表
+    while (header != NULL) {
+        // 创建一个新的header添加到尾部
+        // 并直接返回
+        if (header->next == NULL) {
+            header->next = new_http_header();
+            return header->next;
+        }
+        header = header->next;
+    }
+    // 如果header是空，则创建一个空的header
+    // 并将它赋值给request-headers
+    request->headers = new_http_header();
+    return request->headers;
+}
 
 int on_message_begin(http_parser* parser) {
    (void)parser;
-   printf("\n***MESSAGE BEGIN***\n\n");
+    log_info("\n***MESSAGE BEGIN***\n\n");
    return 0;
 }
 
 int on_headers_complete(http_parser* parser) {
    (void)parser;
-   printf("\n***HEADERS COMPLETE***\n\n");
+    log_info("\n***HEADERS COMPLETE***\n\n");
    return 0;
 }
 
 int on_message_complete(http_parser* parser) {
    (void)parser;
-   printf("\n***MESSAGE COMPLETE***\n\n");
+    log_info("\n***MESSAGE COMPLETE***\n\n");
    return 0;
 }
 
 
 int on_url(http_parser* parser, const char* at, size_t length) {
    (void)parser;
-   printf("Url: %.*s\n", (int)length, at);
+    log_info("Url: %.*s\n", (int)length, at);
 
 
    return 0;
@@ -50,19 +96,19 @@ int on_url(http_parser* parser, const char* at, size_t length) {
 
 int on_header_field(http_parser* parser, const char* at, size_t length) {
    (void)parser;
-   printf("Header field: %.*s\n", (int)length, at);
+    log_info("Header field: %.*s\n", (int)length, at);
    return 0;
 }
 
 int on_header_value(http_parser* parser, const char* at, size_t length) {
    (void)parser;
-   printf("Header value: %.*s\n", (int)length, at);
+    log_info("Header value: %.*s\n", (int)length, at);
    return 0;
 }
 
 int on_body(http_parser* parser, const char* at, size_t length) {
    (void)parser;
-   printf("Body: %.*s\n", (int)length, at);
+    log_info("Body: %.*s\n", (int)length, at);
    return 0;
 }
 
@@ -86,7 +132,6 @@ void handler_request(void *ptr) {
     parser_set.on_body = on_body;
     parser_set.on_headers_complete = on_headers_complete;
     parser_set.on_message_complete = on_message_complete;
-
     http_parser_execute(parser, &parser_set, buf, strlen(buf));
     log_info("size %d\n",recved);
     log_info("aaaaa\n");
--- src/http.h
+++ src/http.h
@@ -4,6 +4,21 @@
 
 #ifndef M_BACK_HTTP_H
 #define M_BACK_HTTP_H
+// 保存HTTP报文头部的字段的链表
+struct http_header {
+    char *name;
+    char *value;
+    struct http_header *next;
+};
+// HTTP请求的结构提
+struct http_request {
+    char *method;
+    char *url;
+    char *body;
+    unsigned int flags;
+    unsigned short http_major, http_minor;
+    struct http_header *headers;
+};
 
 void handler_request(void *ptr);
 
