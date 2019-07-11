6485b7be0d6be27010a1ffb70e2cc66767253e0d
leijin4wk <leijin4wk@163.com>
2019-07-11 14:58:50 +0800

    修改

--- src/ssl_tool.c
+++ src/ssl_tool.c
@@ -1,4 +1,73 @@
 //
 // Created by oyo on 2019-07-11.
 //
+#include "ssl_tool.h"
+
+
+SSL_CTX* init_server_ctx(void)
+{
+    SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    SSL_library_init();                 /* init algorithms library */
+    OpenSSL_add_all_algorithms();       /* load & register all cryptos, etc. */
+    SSL_load_error_strings();           /* load all error messages */
+    //method = SSLv23_server_method();  /* create new server-method instance */
+    method = TLSv1_server_method();
+    ctx = SSL_CTX_new(method);          /* create new context from method */
+    if ( ctx == NULL )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    return ctx;
+}
+void load_certificates(SSL_CTX* ctx, char* CertFile, char* KeyFile)
+{
+    /* set the local certificate from CertFile */
+    if ( SSL_CTX_use_certificate_file(ctx, CertFile, SSL_FILETYPE_PEM) <= 0 )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    /* set server private key password */
+    SSL_CTX_set_default_passwd_cb_userdata(ctx, (void*)PRIKEY_PASSWD);
+    /* set the private key from KeyFile (may be the same as CertFile) */
+    if ( SSL_CTX_use_PrivateKey_file(ctx, KeyFile, SSL_FILETYPE_PEM) <= 0 )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    /* verify private key */
+    if ( !SSL_CTX_check_private_key(ctx) )
+    {
+        fprintf(stderr, "Private key does not match the public certificate\n");
+        abort();
+    }
+    /* set SSL cipher type */
+    SSL_CTX_set_cipher_list(ctx, ALGO_TYPE);
+}
+void show_certs_info(SSL* ssl)
+{
+    X509 *cert;
+    char *line;
+
+    /* Get connect use algorithm type */
+    //printf("SSL connection using %s\n", SSL_get_cipher(ssl));
+    /* Get certificates (if available) */
+    cert = SSL_get_peer_certificate(ssl);
+    if ( cert != NULL )
+    {
+        printf("Server certificates:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        printf("Subject: %s\n", line);
+        free(line);
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        free(line);
+        X509_free(cert);
+    }
+    else
+        printf("No certificates.\n");
+}
 
--- src/ssl_tool.h
+++ src/ssl_tool.h
@@ -4,5 +4,23 @@
 
 #ifndef M_BACK_SSL_TOOL_H
 #define M_BACK_SSL_TOOL_H
+#include <stdio.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+SSL_CTX* init_server_ctx(void);
+
+
+void load_certificates(SSL_CTX* ctx, char* CertFile, char* KeyFile);
+
+
+void show_certs_info(SSL* ssl);
+
+
 
 #endif //M_BACK_SSL_TOOL_H
