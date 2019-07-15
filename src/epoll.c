2017fc9600e235d8c63b9b9e450c6a39478b6c34
leijin4wk <leijin4wk@163.com>
2019-07-15 16:59:28 +0800

    修改

--- src/epoll.c
+++ src/epoll.c
@@ -11,33 +11,33 @@ struct epoll_event *events;
 
 int zv_epoll_create(int flags) {
     int fd = epoll_create1(flags);
-    check(fd > 0, "zv_epoll_create: epoll_create1");
+    check_exit(fd > 0, "zv_epoll_create: epoll_create1");
 
     events = (struct epoll_event *)malloc(sizeof(struct epoll_event) * MAXEVENTS);
-    check(events != NULL, "zv_epoll_create: malloc");
+    check_exit(events == NULL, "zv_epoll_create faill");
     return fd;
 }
 
 void zv_epoll_add(int epfd, int fd, struct epoll_event *event) {
     int rc = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, event);
-    check(rc == 0, "zv_epoll_add: epoll_ctl");
+    check_exit(rc != 0, "zv_epoll_add fial");
     return;
 }
 
 void zv_epoll_mod(int epfd, int fd, struct epoll_event *event) {
     int rc = epoll_ctl(epfd, EPOLL_CTL_MOD, fd, event);
-    check(rc == 0, "zv_epoll_mod: epoll_ctl");
+    check_exit(rc != 0, "zv_epoll_mod fail");
     return;
 }
 
 void zv_epoll_del(int epfd, int fd, struct epoll_event *event) {
     int rc = epoll_ctl(epfd, EPOLL_CTL_DEL, fd, event);
-    check(rc == 0, "zv_epoll_del: epoll_ctl");
+    check_exit(rc != 0, "zv_epoll_del fial");
     return;
 }
 
 int zv_epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout) {
     int n = epoll_wait(epfd, events, maxevents, timeout);
-    check(n >= 0, "zv_epoll_wait: epoll_wait");
+    check_exit(n < 0, "zv_epoll_wait fail!");
     return n;
 }
