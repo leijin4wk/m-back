7e1b5b11f4350e1f433f85810c8c771f7e1eafab
leijin4wk <leijin4wk@163.com>
2019-07-22 20:46:23 +0800

    数据库替换为mysql

--- src/main.c
+++ src/main.c
@@ -5,25 +5,28 @@
 #include "ssl_tool.h"
 #include "db_tool.h"
 #include "socket_tool.h"
+#include "module.h"
 #include "dbg.h"
 #define CONFIG_FILE_PATH "../config.ini"
 int main(){
     init_config(CONFIG_FILE_PATH);
     log_info("config_file load complete!");
-    init_server_ctx();
-    log_info("ssl init complete!");
-    init_connection_pool();
-    log_info("db connection_pool init complete!");
-    int server_fd= init_server_socket();
-    if(server_fd<0){
-        log_err("server socket init fail!");
-        exit(-1);
-    }
+    load_and_init_module();
+////    init_server_ctx();
+////    log_info("ssl init complete!");
+////    init_connection_pool();
+////    log_info("db connection_pool init complete!");
+////    int server_fd= init_server_socket();
+////    if(server_fd<0){
+////        log_err("server socket init fail!");
+////        exit(-1);
+////    }
+////
+////    log_info("server_fd is %d init complete!",server_fd);
+////    ev_loop_init();
+////    ev_accept_start(server_fd);
+////    log_info("ev_loop init complete!");
+////    ev_loop_start();
 
-    log_info("server_fd is %d init complete!",server_fd);
-    ev_loop_init();
-    ev_accept_start(server_fd);
-    log_info("ev_loop init complete!");
-    ev_loop_start();
     return 0;
 }
--- src/module.c
+++ src/module.c
@@ -7,14 +7,18 @@
 #include "dictionary.h"
 #include "iniparser.h"
 #include "str_tool.h"
-#include <regex.h>
+#include "dbg.h"
 extern dictionary* ini_file;
 
 map_void_t dispatcher_map;
 
-
 int load_and_init_module(){
-    const char* modules =iniparser_getint(ini_file,"server:module","");
-
+    int n= iniparser_getsecnkeys(ini_file,"module");
+    log_info("total module : %d",n);
+    char** keys=malloc(sizeof(char*)*n);
+    keys=iniparser_getseckeys(ini_file,"module",keys);
+    for(int i=0;i<n;i++){
+      log_info("%s",keys[i]) ;
+    }
 }
 
