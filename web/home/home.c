00f7e24806f86b16bc0da3c12ab9ad0de25c1aab
leijin <leijin4wk@163.com>
2019-07-26 00:41:18 +0800

    成功完成一个https来回

--- web/home/home.c
+++ web/home/home.c
@@ -6,4 +6,17 @@
 void home_index(struct http_request* request,struct http_response* response)
 {
     log_info("this is home");
+    struct http_header* header=add_http_response_header(response);
+    header->name="Content-Type";
+    header->value="text/html;charset=utf-8";
+    response->body="<!DOCTYPE html>\n"
+                   "<html lang=\"en\">\n"
+                   "<head>\n"
+                   "    <meta charset=\"UTF-8\">\n"
+                   "    <title>Title</title>\n"
+                   "</head>\n"
+                   "<body>\n"
+                   "<h1>这是首页</h1>\n"
+                   "</body>\n"
+                   "</html>";
 }
