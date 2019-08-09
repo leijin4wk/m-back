1906cb7c5b119cfa3edcb973098fc428c8fc29d0
leijin4wk <leijin4wk@163.com>
2019-08-09 15:45:07 +0800

    完善超时删除逻辑，已无bug

--- web/home/home.c
+++ web/home/home.c
@@ -11,11 +11,11 @@ void home_index(struct http_request* request,struct http_response* response)
     header->name=strdup("Content-Type");
     header->value=strdup("text/html;charset=utf-8");
     struct Buffer* body=new_buffer(MAX_READLINE,10*MAX_READLINE);
-    const char* a="<!DOCTYPE html><html lang=\34en\34";
+    const char* a="<!DOCTYPE html><html lang='zh'>";
     buffer_add(body,a,strlen(a));
-    const char* b="<head><meta charset=\34UTF-8\34><title>Title</title></head";
+    const char* b="<head><meta charset='UTF-8'><title>Home</title></head";
     buffer_add(body,b,strlen(b));
-    const char* c="<body><h1>这是首页</h1></body>";
+    const char* c="<body><h1>这是home</h1></body>";
     buffer_add(body,c,strlen(c));
     const char* d="</html>";
     buffer_add(body,d,strlen(d));
